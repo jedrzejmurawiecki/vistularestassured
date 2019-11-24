be20ee6db1aa238af4eb4c951bc11e3ad9273d6e
jedrzejmurawiecki <murawiecki@gmail.com>
2019-11-24 16:48:30 +0100

    24.11

--- src/test/java/org/vistula/restassured/information/InformationControllerTest.java
+++ src/test/java/org/vistula/restassured/information/InformationControllerTest.java
@@ -1,10 +1,15 @@
 package org.vistula.restassured.information;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.vistula.restassured.RestAssuredTest;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.greaterThan;
 
 public class InformationControllerTest extends RestAssuredTest {
 
@@ -16,5 +21,41 @@ public class InformationControllerTest extends RestAssuredTest {
                 .statusCode(200)
                 .body("size()", is(2));
     }
+
+    @Test
+    public void shouldCreateNewPlayer() {
+        JSONObject requestParams = new JSONObject();
+        String myName = RandomStringUtils.randomAlphabetic(10);
+
+
+        requestParams.put("name", myName);
+        String myNationality = "Polish";
+        requestParams.put("nationality", myNationality);
+        int mySalary = 94000;
+        requestParams.put("salary", mySalary);
+
+
+        given().header("Content-Type", "application/json")
+                .body(requestParams.toString())
+                .post("/information")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("name", equalTo(myName))
+                .body("nationality", equalTo(myNationality))
+                .body("salary", equalTo(94000))
+                .body("id", greaterThan(0));
+
+
+
+    }
+    @Test
+    public void deletePlayer() {
+        given().delete("/information/4")
+                .then()
+                .log().all();
+
+    }
+
 
 }
--- src/test/java/org/vistula/restassured/pet/PetControllerTest.java
+++ src/test/java/org/vistula/restassured/pet/PetControllerTest.java
@@ -1,12 +1,17 @@
 package org.vistula.restassured.pet;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.json.JSONObject;
 import org.junit.Test;
 import org.vistula.restassured.RestAssuredTest;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+
 
 public class PetControllerTest extends RestAssuredTest {
 
@@ -21,12 +26,16 @@ public class PetControllerTest extends RestAssuredTest {
 
     @Test
     public void shouldGetFirstPet() {
-        given().get("/pet/1")
+        Pet pet = given().get("/pet/2")
                 .then()
                 .log().all()
                 .statusCode(200)
-                .body("id", is(1))
-                .body("name", equalTo("Cow"));
+                .body("id", is(2))
+                .body("name", equalTo("Dog"))
+                .extract().body().as(Pet.class);
+
+        assertThat(pet.getId()).isEqualTo("2");
+        assertThat(pet.getName()).isEqualTo("Dog");
     }
 
     @Test
@@ -42,5 +51,46 @@ public class PetControllerTest extends RestAssuredTest {
                 .log().all()
                 .statusCode(201);
     }
+    @Test
+    public void shouldntGet100() {
+        int statusCode = given().get("/pet/100")
+                .then()
+                .log().all()
+                .statusCode(404)
+                .body(equalTo("There is not Pet with such id"))
+                .extract().statusCode();
 
+        assertThat(statusCode).isEqualTo(404);
+
+    }
+
+    @Test
+    public void deletePet() {
+                given().delete("/pet/4")
+                .then()
+                .log().all()
+                .statusCode(204);
+
+    }
+
+    @Test
+    public void shouldCreateNewerPet() {
+        JSONObject requestParams = new JSONObject();
+        int value = ThreadLocalRandom.current().nextInt(20, Integer.MAX_VALUE);
+        requestParams.put("id", value);
+        requestParams.put("name", RandomStringUtils.randomAlphabetic(10));
+
+        given().header("Content-Type", "application/json")
+                .body(requestParams.toString())
+                .post("/pet")
+                .then()
+                .log().all()
+                .statusCode(201);
+        given().delete("/pet/"+value)
+                .then()
+                .log().all()
+                .statusCode(204);
+
+
+    }
 }
